101f6d605673969c97abcde78f75c54bf9cc36ac
Ben Smith <benjaminsmith1981@googlemail.com>
2023-06-22 09:20:16 +0000

    added data points

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 window.onload = function() {
+    var dataPoints = [
+        { lat: 37.7749, lng: -122.4194 }, // San Francisco
+        { lat: 34.0522, lng: -118.2437 }, // Los Angeles
+        { lat: 40.7128, lng: -74.0060 } // New York
+      ];
+      
     var map = new google.maps.Map(document.getElementById("map"), {
       center: { lat: 37.7749, lng: -122.4194 },
       zoom: 12
